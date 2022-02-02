aa7fdad710400213d8fd29932f348f040af138d6
Kawtar99 <kawtar.tarib@outlook.fr>
2022-02-02 17:04:32 +0100

    findByValue using streams

--- src/main/java/FindByValueExo.java
+++ src/main/java/FindByValueExo.java
@@ -1,16 +1,14 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class FindByValueExo {
 
     public static List<Integer> findByValue(Map<Integer, String> map, String s){
-        List<Integer> result = new ArrayList<>();
-        for (Integer key : map.keySet()){
-            if (s.equals(map.get(key))){
-                result.add(key);
-            }
-        }
-        return  result;
+        return map.entrySet().stream()
+                .filter(x -> s.equals(x.getValue()))
+                .map(x -> x.getKey())
+                .toList();
     }
 }

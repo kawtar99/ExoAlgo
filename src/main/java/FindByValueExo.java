a71c9993f071086db1a2459c89c6c15846f4f26a
Kawtar99 <kawtar.tarib@outlook.fr>
2022-02-13 21:24:47 +0100

    javadoc

--- src/main/java/FindByValueExo.java
+++ src/main/java/FindByValueExo.java
@@ -5,6 +5,14 @@ import java.util.stream.Collectors;
 
 public class FindByValueExo {
 
+    /**
+     *
+     * @param map : key : an Integer, value: a String
+     * @param s String to search for in the map values
+     * @return a List of keys corresponding to the searched String s,
+     *         The list might be empty if no key corresponding to the String s is found.
+     */
+
     public static List<Integer> findByValue(Map<Integer, String> map, String s){
         return map.entrySet().stream()
                 .filter(x -> s.equals(x.getValue()))

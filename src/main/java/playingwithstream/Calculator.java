5add6d8dd74652ce94f923961f28118fcf3591e6
Kawtar99 <kawtar.tarib@outlook.fr>
2022-02-15 15:20:20 +0100

    add second solution with enum

--- src/main/java/playingwithstream/Calculator.java
+++ src/main/java/playingwithstream/Calculator.java
@@ -3,7 +3,6 @@ package playingwithstream;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
-import java.util.function.BiFunction;
 import java.util.function.BinaryOperator;
 
 public class Calculator {
@@ -22,4 +21,40 @@ public class Calculator {
                 .map((m) -> m.apply(a1, a2))
                 .orElseThrow(UnsupportedOperationException::new);
     }
+
+    public static int calculate2(String operation, int a1, int a2){
+        Optional<Operation> op = Operation.valueOfOperationType(operation);
+        return op.map(x -> x.calculate(a1, a2))
+                .orElseThrow(UnsupportedOperationException::new);
+    }
+}
+
+enum Operation{
+    ADDITION("+", (a,b) -> a+b),
+    SUBSTRACTION ("-", (a,b) -> a-b),
+    MULTIPLICATION("*", (a,b) -> a*b),
+    DIVISION("/", (a,b) -> a/b),
+    ;
+
+    private final String operationType;
+    private final BinaryOperator<Integer> binaryOperator;
+
+    Operation(String operationType, BinaryOperator<Integer> binaryOperator) {
+        this.operationType = operationType;
+        this.binaryOperator = binaryOperator;
+    }
+
+    public int calculate(int a1, int a2) {
+        return binaryOperator.apply(a1, a2);
+    }
+
+    public static Optional<Operation> valueOfOperationType(String operationType) {
+        for (Operation e : Operation.values()) {
+            if (e.operationType.equals(operationType)) {
+                return Optional.of(e);
+            }
+        }
+        return Optional.empty();
+    }
+
 }
--- src/test/java/playingwithstream/CalculatorTest.java
+++ src/test/java/playingwithstream/CalculatorTest.java
@@ -9,36 +9,46 @@ class CalculatorTest {
 
     @Test
     public void shouldAddElements(){
-        int result = Calculator.calculate("+", 1, 3);
+        int result1 = Calculator.calculate("+", 1, 3);
+        int result2 = Calculator.calculate2("+", 1, 3);
 
-        assertEquals(result, 4);
+        assertEquals(result1, 4);
+        assertEquals(result2, 4);
     }
 
     @Test
     public void shouldSubstractElements(){
         int result = Calculator.calculate("-", 1, 3);
-
         assertEquals(result, -2);
+
+        int result2 = Calculator.calculate2("-", 1, 3);
+        assertEquals(result2, -2);
     }
 
     @Test
     public void shouldMultiplyElements(){
         int result = Calculator.calculate("*", 1, 3);
-
         assertEquals(result, 3);
+
+        int result2 = Calculator.calculate2("*", 1, 3);
+        assertEquals(result2, 3);
     }
 
     @Test
     public void shouldDivideElements(){
         int result = Calculator.calculate("/", 4, 2);
-
         assertEquals(result, 2);
+
+        int result2 = Calculator.calculate2("/", 4, 2);
+        assertEquals(result2, 2);
     }
 
     @Test
     public void shouldThrowUnsupportedOperationExceptionWhenPOperator(){
         Assertions.assertThrows(UnsupportedOperationException.class,
                 () -> Calculator.calculate("P", 1, 3));
+        Assertions.assertThrows(UnsupportedOperationException.class,
+                () -> Calculator.calculate2("P", 1, 3));
 
     }
 

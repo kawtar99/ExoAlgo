90aa95fd78cfedbc56039064368dfe647e570173
Kawtar99 <kawtar.tarib@outlook.fr>
2022-02-15 11:43:12 +0100

    playing with streams

--- src/main/java/playingwithstream/PersonTreatment.java
+++ src/main/java/playingwithstream/PersonTreatment.java
@@ -1,7 +1,9 @@
 package playingwithstream;
 
 import java.util.Comparator;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class PersonTreatment {
@@ -16,5 +18,21 @@ public class PersonTreatment {
         return persons.stream()
                 .sorted(Comparator.comparing(Person::getFirstName).thenComparing(Person::getLastName))
                 .collect(Collectors.toList());
+    }
+
+    public Map<String, Long> countOccurences(){
+        var intermediateMap =  persons.stream()
+                .collect(Collectors.groupingBy(
+                        Person::getFirstName,
+                        Collectors.counting()
+                ));
+        return intermediateMap.entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        Map.Entry::getValue,
+                        (e1, e2) -> e1, LinkedHashMap::new
+                ));
     }
 }
--- src/test/java/playingwithstream/PersonTreatmentTest.java
+++ src/test/java/playingwithstream/PersonTreatmentTest.java
@@ -6,6 +6,10 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.entry;
+
 
 class PersonTreatmentTest {
 
@@ -21,5 +25,33 @@ class PersonTreatmentTest {
         List<Person> result = treatment.sort();
 
         Assertions.assertThat(result).containsExactly(p2, p1, p3);
+    }
+
+    @Test
+    public void shouldCount(){
+        Person p1 = new Person("A", "D");
+        Person p2 = new Person("A", "C");
+        Person p3 = new Person("X", "Y");
+
+        var persons = Arrays.asList(p1, p2, p3);
+
+        PersonTreatment treatment = new PersonTreatment(persons);
+        Map<String, Long> firstNamesOccurrences = treatment.countOccurences();
+
+        Assertions.assertThat(firstNamesOccurrences).containsExactly(entry("A", 2L), entry("X", 1L));
+    }
+
+    @Test
+    public void shouldCount2(){
+        Person p1 = new Person("X", "C");
+        Person p2 = new Person("A", "C");
+        Person p3 = new Person("X", "Y");
+
+        var persons = Arrays.asList(p1, p2, p3);
+
+        PersonTreatment treatment = new PersonTreatment(persons);
+        Map<String, Long> firstNamesOccurrences = treatment.countOccurences();
+
+        Assertions.assertThat(firstNamesOccurrences).containsExactly(entry("X", 2L), entry("A", 1L));
     }
 }

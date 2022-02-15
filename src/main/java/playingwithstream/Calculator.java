ecf11bfdfa4cb3cc018cfd4937b992bde5852707
Kawtar99 <kawtar.tarib@outlook.fr>
2022-02-15 12:25:42 +0100

    static bloc static bloc

--- src/main/java/playingwithstream/Calculator.java
+++ src/main/java/playingwithstream/Calculator.java
@@ -8,16 +8,18 @@ import java.util.function.BinaryOperator;
 
 public class Calculator {
 
-    public static int calculate(String operation, int a1, int a2){
-        Map<String, BinaryOperator<Integer>> operations = new HashMap<>();
+    public static final Map<String, BinaryOperator<Integer>> operations = new HashMap<>();
+    static {
         operations.put("+", (a,b) -> a+b);
         operations.put("*", (a,b) -> a*b);
         operations.put("-", (a,b) -> a-b);
         operations.put("/", (a,b) -> a/b);
+    }
 
+
+    public static int calculate(String operation, int a1, int a2){
         return Optional.ofNullable(operations.get(operation))
                 .map((m) -> m.apply(a1, a2))
                 .orElseThrow(UnsupportedOperationException::new);
-
     }
 }

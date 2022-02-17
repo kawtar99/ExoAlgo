133bbc3a84c41d2cad1df580cfd978ba78976a34
Kawtar99 <kawtar.tarib@outlook.fr>
2022-02-17 10:58:33 +0100

    refactoring

--- src/main/java/playingwiththread/ExecutorServiceExample.java
+++ src/main/java/playingwiththread/ExecutorServiceExample.java
@@ -7,32 +7,39 @@ public class ExecutorServiceExample {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
        ExecutorService executorService = Executors.newFixedThreadPool(2);
 
-       executorService.execute(new Runnable() {
-           @Override
-           public void run() {
-               System.out.println("Inside execute method " +
-                       "Called with a Runnable");
+       executorService.execute(() -> {
+           System.out.println("Inside " + Thread.currentThread().getName() + " execute method called with a Runnable");
+           try {
+               TimeUnit.MILLISECONDS.sleep(1000);
+           } catch (InterruptedException e) {
+               e.printStackTrace();
            }
        });
 
-       Future<String> resCallable = executorService.submit(new Callable<String>() {
 
-           @Override
-           public String call() throws Exception {
-               System.out.println("Inside submit method");
-               return "Invoked with a Callable";
+       Future<String> resCallable = executorService.submit(() -> {
+           System.out.println("Inside " + Thread.currentThread().getName() + " submit method");
+           try {
+               TimeUnit.MILLISECONDS.sleep(2000);
+           } catch (InterruptedException e) {
+               e.printStackTrace();
+           }
+           return "Invoked with a Callable";
+       });
+
+       Future<?> resRunnable = executorService.submit(() -> {
+           System.out.println("Inside " +  Thread.currentThread().getName() + " submit method Called with a Runnable");
+           try {
+               TimeUnit.MILLISECONDS.sleep(3000);
+           } catch (InterruptedException e) {
+               e.printStackTrace();
            }
        });
 
-       Future<?> resRunnable = executorService.submit(new Runnable() {
-           @Override
-           public void run() {
-               System.out.println("Inside submit method " +
-                       "Called with a Runnable");
-           }
-       });
-
+       // Callable result is a String
        System.out.println("The result for Callable is : " + resCallable.get());
+
+       // Runnable result returns null
        System.out.println("The result for Runnable is : " + resRunnable.get());
     }
 }
